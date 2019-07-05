41656eec7f9313535e1606d1fceaa44efa41b435
Dillan Teagle <dillanteagle@Macs-MacBook-Pro.local>
2019-07-05 12:18:37 -0500

    initialize admin interface for api

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+
+admin = Admin(app)
 
 from views import *
 
--- models.py
+++ models.py
@@ -1,4 +1,16 @@
-from application import db
+from application import db, admin
+from flask_admin.contrib.sqla import ModelView
+
+
+class Device(db.Model):
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(50), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+
+    def __repr__(self):
+        return '<Id: %r, Name: %s>' % (self.id, self.name)
+
+admin.add_view(ModelView(Device, db.session))
 
 
 class Organization(db.Model):
@@ -9,7 +21,9 @@ class Organization(db.Model):
     password        = db.Column(db.String(80))
     admin           = db.Column(db.Boolean)
 
-class Device(db.Model):
-    id              = db.Column(db.Integer, primary_key=True)
-    name            = db.Column(db.String(50), nullable=False)
-    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    def __repr__(self):
+        return '<Id: %r, Name: %s>' % (self.id, self.name)
+
+admin.add_view(ModelView(Organization, db.session))
+
+
